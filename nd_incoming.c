267471ddeb690c9d5128e36e11529b9c5d198a54
qizhe <qizhee.cai@gmail.com>
2020-11-19 12:04:49 -0500

    remove debug statement

--- nd_incoming.c
+++ nd_incoming.c
@@ -1030,7 +1030,6 @@ int nd_handle_ack_pkt(struct sk_buff *skb) {
 	// sk = __nd_lookup_skb(&nd_hashinfo, skb, __nd_hdrlen(&ah->common), ah->common.source,
     //         ah->common.dest, sdif, &refcounted);
     // }
-	pr_info("receive ack: %u\n", ntohl(ah->grant_seq));
 	// if(sk) {
  	// 	bh_lock_sock(sk);
 	// 	dsk = nd_sk(sk);
