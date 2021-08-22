d3044ef5c3af5620eb6b8dd891a4657204cdeceb
qizhe <qizhe.cai@gmail.com>
2021-08-21 23:48:27 -0400

    add change

--- run_single_flow_set_up.sh
+++ run_single_flow_set_up.sh
@@ -1,4 +1,4 @@
-sudo sysctl  net.nd.nd_default_sche_policy=9
+sudo sysctl  net.nd.nd_default_sche_policy=0
 sudo sysctl  net.nd.nd_num_dc_thread=1
 sudo sysctl  net.nd.wmem_default=4289600
 sudo sysctl  net.nd.rmem_default=6289600
