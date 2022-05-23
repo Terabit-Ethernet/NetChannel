af6ff00c3f92f73f2d0db8eaae75e14fa24b92a4
qizhe <qizhe.cai@gmail.com>
2022-05-23 12:53:15 -0400

    update script for rx flow size

--- run_np.sh
+++ run_np.sh
@@ -4,6 +4,7 @@ sudo sysctl  net.nd.nd_num_dc_thread=0
 sudo sysctl  net.nd.wmem_default=589600
 sudo sysctl  net.nd.rmem_default=6289600
 # change back to default
+sudo ethtool -G ens2f0 rx 256
 sudo sysctl -w net.ipv4.tcp_rmem='4096 131072 1291456'
 sudo ethtool -K ens2f0 tso off gso off gro off lro off
 sudo ifconfig ens2f0 mtu 1500
--- run_single_flow_set_up.sh
+++ run_single_flow_set_up.sh
@@ -1,6 +1,7 @@
 sudo sysctl  net.nd.nd_default_sche_policy=0
 sudo sysctl  net.nd.num_thpt_channels=1
 sudo sysctl  net.nd.nd_num_dc_thread=1
+sudo ethtool -G ens2f0 rx 256
 sudo sysctl  net.nd.wmem_default=3289600
 sudo sysctl  net.nd.rmem_default=3000000
 sudo sysctl -w net.ipv4.tcp_rmem='4096 131072 3000000'
