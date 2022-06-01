6c793bac9ec55850fe468c77a66e8a8f9843d8ca
qizhe <qizhe.cai@gmail.com>
2022-06-01 13:08:25 -0400

    add network_setup.sh

--- scripts/network_setup.sh
+++ scripts/network_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 device_ip=${1:-192.168.10.116}
 iface=${2:-ens2f0}
+sudo ip link set arp off dev $iface ; ip link set arp on dev $iface
 sudo ethtool -G $iface rx 1024
 #sudo sysctl -w net.ipv4.tcp_rmem='4096 131072 6000000'
 echo 0 | sudo tee /proc/sys/kernel/sched_autogroup_enabled
