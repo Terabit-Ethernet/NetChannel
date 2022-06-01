0a856e00c9ca5b9fddf6409a8f6ea18c3277645a
qizhe <qizhe.cai@gmail.com>
2022-06-01 13:25:11 -0400

    add run_client.sh

--- util/run_client.sh
+++ util/run_client.sh
@@ -1,8 +1,9 @@
 flows=$1
+protocol=$2
 flow=0
 while (( flow < flows ));do
 	((core=16+4*flow))
 	((port=4000+flow))
-	taskset -c 28 ./netdriver_test 192.168.10.117:$port --sp $((6000+core)) --count 1 ndping &
+	taskset -c 28 ./netdriver_test 192.168.10.117:$port --sp $((6000+core)) --count 1 "$protocol"ping &
 	((flow++))
 done
