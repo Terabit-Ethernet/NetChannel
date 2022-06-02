26cd64662672cd839d5fbd6660ab94322ffcf08d
Jaehyun Hwang <bekind@gmail.com>
2022-06-02 11:24:29 -0400

    update fig6c

---
+++ sigcomm22_artifact/fig6c-nc-client.sh
@@ -0,0 +1,14 @@
+source param.sh
+
+# Configuration
+sudo ~/NetChannel/scripts/run_np.sh $iface $1
+
+# Run the server program
+flows=1
+protocol='nd'
+nice=-20
+i=0
+while (( i < flows ));do
+	sudo nice -n $nice taskset -c 0 ~/NetChannel/util/netdriver_test_multithread $server_ip:$((6000+i%8)) --sp $((10000+i)) --count 1 "$protocol"ppasync
+    (( i = 1 + i ))
+done
---
+++ sigcomm22_artifact/fig6c-nc-uring-server.sh
@@ -0,0 +1,7 @@
+source param.sh
+
+# Configuration
+sudo ~/NetChannel/scripts/run_np.sh $iface $1
+
+# Run the server program
+sudo taskset -c 28 ~/NetChannel/util/iouring_bench_nc server $server_ip 9095
