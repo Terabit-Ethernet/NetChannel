5ebd30b27c336784a6111c221368f1fcc7c34b92
Jaehyun Hwang <bekind@gmail.com>
2022-06-02 04:49:02 -0400

    update toy example

--- util/run_client.sh
+++ util/run_client.sh
@@ -1,9 +1,10 @@
-flows=$1
-protocol=$2
+server_ip=${1:-192.168.10.117}
+flows=${2:-1}
+protocol=${3:-nd}
 flow=0
 while (( flow < flows ));do
 	((core=16+4*flow))
 	((port=4000+flow))
-	taskset -c 28 ./netdriver_test 192.168.10.117:$port --sp $((6000+core)) --count 1 "$protocol"ping &
+	taskset -c 28 ./netdriver_test $server_ip:$port --sp $((6000+core)) --count 1 "$protocol"ping &
 	((flow++))
 done
--- util/run_single_server.sh
+++ util/run_single_server.sh
@@ -1,8 +1,9 @@
-flows=$1
+server_ip=${1:-192.168.10.117}
+flows=${2:-1}
 #core_id=12
 flow=0
 while (( flow < flows ));do
         ((core=flow%4*4+16))
-        taskset -c 28 ./server --ip 192.168.10.117 --port $((4000 + flow)) &
+        taskset -c 28 ./server --ip $server_ip --port $((4000 + flow)) &
         (( flow++ ))
 done
