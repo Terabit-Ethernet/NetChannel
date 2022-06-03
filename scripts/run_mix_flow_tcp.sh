4288da0284c39cfc3b1d9c082479e283dd26bb0d
Jaehyun Hwang <bekind@gmail.com>
2022-06-03 11:19:18 -0400

    update fig6d-tcp

--- scripts/run_mix_flow_tcp.sh
+++ scripts/run_mix_flow_tcp.sh
@@ -1,10 +1,11 @@
+iface=${1:-ens2f0}
 sudo sysctl  net.nd.nd_default_sche_policy=1
 sudo sysctl  net.nd.num_thpt_channels=4
 sudo sysctl  net.nd.nd_num_dc_thread=0
 sudo sysctl  net.nd.wmem_default=589600
 sudo sysctl  net.nd.rmem_default=689600
 # change back to default
-sudo ethtool -G ens2f0 rx 1024
+sudo ethtool -G $iface rx 1024
 sudo sysctl -w net.ipv4.tcp_rmem='4096 131072 6291456'
-sudo ethtool -K ens2f0 tso on gso on gro on lro off
-sudo ifconfig ens2f0 mtu 9000
+sudo ethtool -K $iface tso on gso on gro on lro off
+sudo ifconfig $iface mtu 9000
