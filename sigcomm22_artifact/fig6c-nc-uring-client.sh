f262f0f649e15162a043e7637543db24ef3ff4a2
Jaehyun Hwang <bekind@gmail.com>
2022-06-02 21:20:36 -0400

    minor update

--- sigcomm22_artifact/fig6c-nc-uring-client.sh
+++ sigcomm22_artifact/fig6c-nc-uring-client.sh
@@ -4,4 +4,4 @@ source param.sh
 sudo ~/NetChannel/scripts/run_np.sh $iface $1
 
 # Run the client program
-sudo taskset -c ~/NetChannel/util/iouring_bench_nc client-shortflows $server_ip 9095 60
+sudo taskset -c 0 ~/NetChannel/util/iouring_bench_nc client-shortflows $server_ip 9095 60
