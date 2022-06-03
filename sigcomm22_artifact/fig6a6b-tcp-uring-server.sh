36d464af8f622e8df0be3d153fbbb6f2c903c068
Jaehyun Hwang <bekind@gmail.com>
2022-06-03 10:34:00 -0400

    update fig6a6b-tcp

---
+++ sigcomm22_artifact/fig6a6b-tcp-uring-server.sh
@@ -0,0 +1,7 @@
+source param.sh
+
+# Configuration
+sudo ~/NetChannel/scripts/run_single_flow_set_up_tcp.sh $iface
+
+# Run the server program
+sudo taskset -c 28 ~/NetChannel/util/iouring_bench server $server_ip 9095
