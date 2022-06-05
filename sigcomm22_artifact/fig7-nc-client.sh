18f71d81ddfb9ee3a2643b95efeb4b6f4f6fb843
Jaehyun Hwang <bekind@gmail.com>
2022-06-05 11:31:37 -0400

    update fig7-nc

--- sigcomm22_artifact/fig7-nc-client.sh
+++ sigcomm22_artifact/fig7-nc-client.sh
@@ -4,12 +4,13 @@ source param.sh
 sudo ~/NetChannel/scripts/run_np.sh $iface
 
 # Run the client program
+echo ""
 echo "### Run redis_populate ###"
 ~/redis/redis_populate
 echo ""
 
-for qd in 1 2 4 8 16 32 64 128 256 512; do
-	echo "### Queue depth = $qd"
+for qd in 1 2 4 8 16 32 64; do
+	echo "### Queue depth = $qd ###"
 	taskset -c 0-31:4 ~/redis/redis_async 192.168.10.117 6379 8 0.75 1 $qd
 	echo ""
 done
