69c0f13b0f860a0976fb59138c86f47977546f68
Jaehyun Hwang <jh.hwang@skku.edu>
2022-06-12 08:48:19 -0400

    update sar commd

--- sigcomm22_artifact/fig2-irq-tcp-uring-client.sh
+++ sigcomm22_artifact/fig2-irq-tcp-uring-client.sh
@@ -6,8 +6,8 @@ source param.sh
 # Run the client program
 sudo taskset -c 28 ~/NetChannel/util/iouring_bench client $server_ip 9095 60 > thru.log &
 
-# Measure CPU utilization
-ssh -t $server_ip 'sar -u 55 1 -P 4,28' > cpu_server.log &
+# [Note] we are measuring the utilization of NUMA0 cores:
+ssh -t $server_ip 'sar -u 55 1 -P 0,4,8,12,16,20,24,28' > cpu_server.log &
 ssh -t $server_ip 'sudo /usr/src/linux-5.4.43/tools/perf/perf record -F 99 -a -- sleep 55'
 
 sleep 7
@@ -17,7 +17,13 @@ ssh -t $server_ip 'cat perf.log' > perf.log
 ssh -t $server_ip 'sudo rm perf.data perf.log'
 
 thru=$(grep Throughput: thru.log | awk '{x=x+$2;} END {print x;}')
-grep 'Average:          4' cpu_server.log > cpu_server2.log
+grep 'Average:          0' cpu_server.log > cpu_server2.log
+grep 'Average:          4' cpu_server.log >> cpu_server2.log
+grep 'Average:          8' cpu_server.log >> cpu_server2.log
+grep 'Average:         12' cpu_server.log >> cpu_server2.log
+grep 'Average:         16' cpu_server.log >> cpu_server2.log
+grep 'Average:         20' cpu_server.log >> cpu_server2.log
+grep 'Average:         24' cpu_server.log >> cpu_server2.log
 grep 'Average:         28' cpu_server.log >> cpu_server2.log
 cpu_server=$(grep Average: cpu_server2.log | awk '{x=x+$3+$5;} END {print x/100.0;}')
 
